868f989cb4e7dd8da1aed32a4642c1b7cd2fb4a5
keisei803 <keisei803@gmail.com>
2015-11-18 13:43:56 +0800

    database renew

--- ise_pdt/admin.py
+++ ise_pdt/admin.py
@@ -1,17 +1,33 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Project, TimeLog
+from .models import *
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('user_name', 'user_email', 'user_password')
+    list_display = ('username', 'email', 'password')
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('project_name', 'creating_user', 'project_creation_date', 'project_total_time')
+    list_display = ('project_name', 'creating_user', 'project_creation_date', 
+                    'project_total_time', 'project_yield')
 
 class TimeLogAdmin(admin.ModelAdmin):
-    list_display = ('user', 'project', 'time_worked')
+    list_display = ('user', 'project', 'log_creation_date', 'time_worked', 'work_type')
 
-admin.site.register(User, UserAdmin)
+class IterationAdmin(admin.ModelAdmin):
+    list_display = ('project', 'phase', 'iteration_name', 'iteration_start_date',
+    	            'iteration_status', 'iteration_estimate_SLOC', 'iteration_SLOC',
+                    'iteration_estimate_effort', 'iteration_effort',
+    	            'iteration_defect_injected', 'iteration_defect_removed')
+
+class PhaseAdmin(admin.ModelAdmin):
+    list_display = ('project', 'phase_name', 'phase_start_date', 'phase_status')
+
+#class WorkAdmin(admin.ModelAdmin):
+#    list_display = ('work_name',)
+
+#admin.site.register(User, UserAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(TimeLog, TimeLogAdmin)
+admin.site.register(Iteration, IterationAdmin)
+admin.site.register(Phase, PhaseAdmin)
+#admin.site.register(Work, WorkAdmin)
--- ise_pdt/models.py
+++ ise_pdt/models.py
@@ -1,31 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class User(models.Model):
-    user_name = models.CharField(max_length=20)
-    user_email = models.CharField(max_length=100)
-    user_password = models.CharField(max_length=20)
-
-    def __str__(self):              # __unicode__ on Python 2
-        return self.user_name
-
 
 class Project(models.Model):
     creating_user = models.ForeignKey(User)
     project_name = models.CharField(max_length=20)
     project_creation_date = models.DateTimeField('date published')
     project_total_time = models.IntegerField(default=0)
-    user_in_project = models.CharField(max_length=20)
+    project_yield = models.FloatField(default=0)
+    #project_members = models.ManyToManyField(User, through='Assignment') 
     def __str__(self):              # __unicode__ on Python 2
         return self.project_name
 
-
 class TimeLog(models.Model):
+    WORK_CHOICE = (
+        ('DEV', 'Development'),
+        ('DEF', 'Defect removal'),
+        ('MAN', 'Management'),
+    )
+    #assignment = models.ForeignKey(Assignment)
     user = models.ForeignKey(User)
     project = models.ForeignKey(Project)
     log_creation_date = models.DateTimeField('date published')
-    time_worked = models.IntegerField(default=0)
-
+    time_worked = models.DurationField()
+    work_type = models.CharField(max_length=3,choices=WORK_CHOICE,default='DEV')
     def __str__(self):              # __unicode__ on Python 2
-        return self.time_worked
+        return self.work_type
+
+class Phase(models.Model):
+    STATUS_CHOICE = (
+        ('ACT', 'Active'),
+        ('CLS', 'Closed'),
+        ('PND', 'Pending'),
+    )
+    PHASE_CHOICE = (
+        ('INCP', 'Inception'),
+        ('ELAB', 'Elaboration'),
+        ('CONS', 'Sonstruction'),
+        ('TRAN', 'Transition'),
+    )
+    project = models.ForeignKey(Project)
+    phase_name = models.CharField(max_length=4,choices=PHASE_CHOICE)
+    phase_start_date = models.DateTimeField('date published')
+    phase_status = models.CharField(max_length=3,choices=STATUS_CHOICE)
+    def __str__(self):              # __unicode__ on Python 2
+        return self.phase_name
+
+class Iteration(models.Model):
+    STATUS_CHOICE = (
+        ('ACT', 'Active'),
+        ('CLS', 'Closed'),
+        ('PND', 'Pending'),
+    )
+    project = models.ForeignKey(Project)
+    phase = models.ForeignKey(Phase)
+    iteration_name = models.CharField(max_length=20)
+    iteration_start_date = models.DateTimeField('date published')
+    iteration_status = models.CharField(max_length=3,choices=STATUS_CHOICE)
+    iteration_estimate_SLOC = models.IntegerField(default=0)
+    iteration_SLOC = models.IntegerField(default=0)
+    iteration_estimate_effort = models.FloatField()
+    iteration_effort = models.FloatField()
+    iteration_defect_injected = models.IntegerField(default=0)
+    iteration_defect_removed = models.IntegerField(default=0)
+    def __str__(self):              # __unicode__ on Python 2
+        return self.iteration_name
