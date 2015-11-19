1916bd998013e58eecacdebe0b9983d4733f5f41
PrakharR <pr_revent@hotmail.com>
2015-11-19 12:33:13 +0800

    Commit at 12:33pm - Thursday

--- ise_pdt/views.py
+++ ise_pdt/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import Project, TimeLog, Phase, Iteration
 
+
 def login_user(request):
     logout(request)
     username = password = ''
@@ -23,31 +24,60 @@ def login_user(request):
                 else:
                   return HttpResponseRedirect('/ise_pdt/manager/'+user.username+'/')
     return render(request, 'ise_pdt/login.html')
+  
+  
+def logout_user(request):
+  logout(request)
+  return HttpResponseRedirect('/ise_pdt/login')
 
+  
 def index(request, username):
     list_of_projects = Project.objects.all().order_by('project_creation_date')
     context = {'list_of_projects': list_of_projects, 'username': username}
-    return render(request, 'ise_pdt/index.html', context)
+    if request.user.is_authenticated() and request.user.groups.filter(name='Developer').exists():
+      return render(request, 'ise_pdt/index.html', context)
+    else:
+      return HttpResponseRedirect('/ise_pdt/logout')
+    
   
-def index_m(request, user_name):
-    list_of_projects = Project.objects.filter(user_in_project = user_name).order_by('project_creation_date')
-    context = {'list_of_projects': list_of_projects, 'user_name': user_name}
-    return render(request, 'ise_pdt/index_m.html', context)
+def index_m(request, username):
+    list_of_projects = Project.objects.all().order_by('project_creation_date')
+    context = {'list_of_projects': list_of_projects, 'username': username}
+    if request.user.is_authenticated() and request.user.groups.filter(name='Manager').exists():
+      return render(request, 'ise_pdt/index_m.html', context)
+    else:
+      return HttpResponseRedirect('/ise_pdt/logout')
   
-
 
 def project(request, username, project_id):
     the_project = Project.objects.filter(id = project_id)
     the_user = User.objects.filter(username = username)
     list_of_projects = Project.objects.all().order_by('project_creation_date')
-
     context = {'list_of_projects': list_of_projects, 'the_project': the_project, 'the_user': the_user}
-    return render(request, 'ise_pdt/project.html', context)
+    if request.user.is_authenticated() and request.user.groups.filter(name='Developer').exists():
+      return render(request, 'ise_pdt/project.html', context)
+    else:
+      return HttpResponseRedirect('/ise_pdt/logout')
   
-def project_m(request, user_name, project_id):
+  
+def project_m(request, username, project_id):
     the_project = Project.objects.filter(id = project_id)
-    the_user = User.objects.filter(user_name = user_name)
-    list_of_projects = Project.objects.filter(user_in_project = user_name).order_by('project_creation_date')
-
+    the_user = User.objects.filter(username = username)
+    list_of_projects = Project.objects.all().order_by('project_creation_date')
     context = {'list_of_projects': list_of_projects, 'the_project': the_project, 'the_user': the_user}
-    return render(request, 'ise_pdt/project.html', context)
+    if request.user.is_authenticated() and request.user.groups.filter(name='Manager').exists():
+      return render(request, 'ise_pdt/project_m.html', context)
+    else:
+      return HttpResponseRedirect('/ise_pdt/logout')
+
+    
+def iteration_m(request, username, project_id, iteration_id):
+    the_project = Project.objects.filter(id = project_id)
+    the_user = User.objects.filter(username = username)
+    the_iteration = Iteration.objects.filter(id = iteration_id)
+    list_of_projects = Project.objects.all().order_by('project_creation_date')
+    context = {'list_of_projects': list_of_projects, 'the_project': the_project, 'the_user': the_user, 'the_iteration': the_iteration}
+    if request.user.is_authenticated() and request.user.groups.filter(name='Manager').exists():
+      return render(request, 'ise_pdt/iteration_m.html', context)
+    else:
+      return HttpResponseRedirect('/ise_pdt/logout')
